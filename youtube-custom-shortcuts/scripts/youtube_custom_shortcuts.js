08530b4e5c446aba11d7b1e47d05e0f0e6580182
Tuna <tunailgaz@gmail.com>
2024-05-10 20:14:12 +0300

    wip

--- youtube-custom-shortcuts/scripts/youtube_custom_shortcuts.js
+++ youtube-custom-shortcuts/scripts/youtube_custom_shortcuts.js
@@ -4,27 +4,16 @@ which is annoying... This script adds custom shortcuts to YouTube video player.
  ArrowLeft/ArrowRight: Focus on play button
 */
 
-function getPlayButton() {
-  return document.querySelector('.ytp-play-button.ytp-button');
-}
-
-function getSoundButton() {
-  return document.querySelector('.ytp-mute-button.ytp-button');
-}
-
-function getSearchButton() {
-  return document.querySelector('form#search-form input#search');
-}
 
 function addShortcuts(play_btn, sound_btn, search_btn) {
   console.log('Adding custom shortcuts to YouTube video player')
   document.addEventListener('keydown', function (event) {
-    if(search_btn !== document.activeElement) {
+    if (search_btn !== document.activeElement) {
       if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
         sound_btn.focus()
       } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
         play_btn.focus()
-      } else if (event.key === ' '){
+      } else if (event.key === ' ') {
         sound_btn.focus() // To prevent 'space' key from toggling play/pause
         play_btn.click()
       }
@@ -32,28 +21,44 @@ function addShortcuts(play_btn, sound_btn, search_btn) {
   });
 }
 
+function querySelectorButtons() {
+  return {
+    play_btn: document.querySelector('.ytp-play-button.ytp-button'),
+    sound_btn: document.querySelector('.ytp-mute-button.ytp-button'),
+    search_btn: document.querySelector('form#search-form input#search')
+  }
+}
+
+function getButtonsAsync() {
+  return new Promise((resolve, reject) => {
+    const {play_btn, sound_btn, search_btn} = querySelectorButtons()
+    if (play_btn && sound_btn && search_btn) {
+      resolve({play_btn, sound_btn, search_btn})
+    } else {
+      let timeout_count = 0;
+      let interval = setInterval(() => {
+        timeout_count++;
+        const {play_btn, sound_btn, search_btn} = querySelectorButtons()
+        if (play_btn && sound_btn && search_btn) {
+          clearInterval(interval)
+          resolve({play_btn, sound_btn, search_btn})
+        }
+        if (timeout_count > 100) {
+          console.log('Could not find play button and sound button')
+          clearInterval(interval)
+          reject('Could not find play button and sound button')
+        }
+      }, 150)
+    }
+  })
+}
+
 window.addEventListener('load', () => {
   console.log('Youtube custom shortcuts script loaded')
-  let play_btn = getPlayButton()
-  let sound_btn = getSoundButton()
-  let search_btn = getSearchButton()
-  if (play_btn && sound_btn && search_btn) {
-    addShortcuts(play_btn, sound_btn, search_btn);
-  } else {
-    let timeout_count = 0;
-    let interval = setInterval(() => {
-      timeout_count++;
-      play_btn = getPlayButton()
-      sound_btn = getSoundButton()
-      search_btn = getSearchButton()
-      if (play_btn && sound_btn && search_btn) {
-        clearInterval(interval)
-        addShortcuts(play_btn, sound_btn, search_btn)
-      }
-      if (timeout_count > 30) {
-        console.log('Could not find play button and sound button')
-        clearInterval(interval)
-      }
-    }, 500)
-  }
+  getButtonsAsync().then(({play_btn, sound_btn, search_btn}) => {
+    addShortcuts(play_btn, sound_btn, search_btn)
+  }).catch((error) => {
+    console.log(error)
+  })
 });
+
