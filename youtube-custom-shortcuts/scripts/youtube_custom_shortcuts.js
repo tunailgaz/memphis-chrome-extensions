87fddce57f5055affce7c2b3a6a29feaa17f824d
Tuna <tunailgaz@gmail.com>
2024-05-10 20:57:14 +0300

    refactor: tried new github copilot, and already hated my own code LMAO, this is much better and make sense version of adding custom shortcuts

--- youtube-custom-shortcuts/scripts/youtube_custom_shortcuts.js
+++ youtube-custom-shortcuts/scripts/youtube_custom_shortcuts.js
@@ -1,64 +1,58 @@
-/* By default, if 'focus' on video progress, up/down keys act forward/backward,
-which is annoying... This script adds custom shortcuts to YouTube video player.
- ArrowUp/ArrowDown: Focus on sound button
- ArrowLeft/ArrowRight: Focus on play button
-*/
+/**
+ * This function creates an observer that adds custom keyboard shortcuts to YouTube.
+ * It observes the document for changes and adds the shortcuts when the necessary elements are available.
+ * Once the shortcuts are added, it disconnects the observer to prevent unnecessary checks.
+ * The shortcuts are only added once per page load.
+ */
+function createShortcutsObserver() {
+  // Flag to check if the shortcuts have already been added
+  let shortcutsAdded = false;
 
+  /**
+   * The observer checks for changes in the document.
+   * If the shortcuts have not been added yet, it tries to add them.
+   */
+  const observer = new MutationObserver(() => {
+    // If the shortcuts have already been added, we don't need to do anything
+    if (shortcutsAdded) return;
 
-function addShortcuts(play_btn, sound_btn, search_btn) {
-  console.log('Adding custom shortcuts to YouTube video player')
-  document.addEventListener('keydown', function (event) {
-    if (search_btn !== document.activeElement) {
-      if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-        sound_btn.focus()
-      } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-        play_btn.focus()
-      } else if (event.key === ' ') {
-        sound_btn.focus() // To prevent 'space' key from toggling play/pause
-        play_btn.click()
-      }
+    // Get the necessary elements from the page
+    const playButton = document.querySelector('.ytp-play-button.ytp-button');
+    const soundButton = document.querySelector('.ytp-mute-button.ytp-button');
+    const searchButton = document.querySelector('form#search-form input#search');
+
+    // If all the necessary elements are available, we can add the shortcuts
+    if (playButton && soundButton && searchButton) {
+
+      // Add the event listener for the keyboard shortcuts
+      document.addEventListener('keydown', (event) => {
+        // If the search field is not focused
+        if (searchButton !== document.activeElement) {
+          // If the user presses the up or down arrow, focus the sound button
+          if (['ArrowUp', 'ArrowDown'].includes(event.key)) {
+            soundButton.focus();
+          }
+          // If the user presses the left or right arrow or space, focus the play button
+          else if (['ArrowLeft', 'ArrowRight', ' '].includes(event.key)) {
+            playButton.focus();
+            // If the user presses space, also click the play button
+            if (event.key === ' ') playButton.click();
+          }
+        }
+      });
+
+      // Set the flag to true as the shortcuts have been added
+      shortcutsAdded = true;
+
+      // Disconnect the observer as we don't need it anymore
+      observer.disconnect();
+      console.log('YouTube Custom Shortcuts: Adding shortcuts, disabling observer')
     }
   });
+
+  // Start observing the document for changes
+  observer.observe(document, {childList: true, subtree: true});
 }
 
-function querySelectorButtons() {
-  return {
-    play_btn: document.querySelector('.ytp-play-button.ytp-button'),
-    sound_btn: document.querySelector('.ytp-mute-button.ytp-button'),
-    search_btn: document.querySelector('form#search-form input#search')
-  }
-}
-
-function getButtonsAsync() {
-  return new Promise((resolve, reject) => {
-    const {play_btn, sound_btn, search_btn} = querySelectorButtons()
-    if (play_btn && sound_btn && search_btn) {
-      resolve({play_btn, sound_btn, search_btn})
-    } else {
-      let timeout_count = 0;
-      let interval = setInterval(() => {
-        timeout_count++;
-        const {play_btn, sound_btn, search_btn} = querySelectorButtons()
-        if (play_btn && sound_btn && search_btn) {
-          clearInterval(interval)
-          resolve({play_btn, sound_btn, search_btn})
-        }
-        if (timeout_count > 100) {
-          console.log('Could not find play button and sound button')
-          clearInterval(interval)
-          reject('Could not find play button and sound button')
-        }
-      }, 150)
-    }
-  })
-}
-
-window.addEventListener('load', () => {
-  console.log('Youtube custom shortcuts script loaded')
-  getButtonsAsync().then(({play_btn, sound_btn, search_btn}) => {
-    addShortcuts(play_btn, sound_btn, search_btn)
-  }).catch((error) => {
-    console.log(error)
-  })
-});
-
+// Call the function to start the observer
+createShortcutsObserver();
